853050b029a94022858c48127a8b727235e3bbf2
thachpham2909 <thachyyy@gmail.com>
2024-02-24 10:56:37 +0700

    create Model for categories

---
+++ app/models/categories.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import enum
+from sqlalchemy import Column, Enum, Integer, String, Date, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from .base import Base  # Assuming .base is the correct import path for your Base
+class Status(str,enum.Enum):
+    ACTIVE = "ACTIVE" #Đang kinh doanh
+    INACTIVE = "INACTIVE" #Tạm ngừng kinh doanh
+    PENDING = "PENDING" #Hết hàng
+    EMPTY = "EMPTY" #Trống
+class categories(Base):
+    __tablename__ = 'categories'
+
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    name = Column(String(255),nullable=False)
+    description = Column(String(255),nullable=True)
+    has_promotion = Column(Boolean, nullable=False,default="No")
+    
