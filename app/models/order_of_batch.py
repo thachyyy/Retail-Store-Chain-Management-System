a30b2c24c9786b60e11d83629f94585725d60fed
ChauNhan0201 <minhchaunhan01@gmail.com>
2024-02-26 10:12:42 +0700

    [new][26/02/2024] model for order of batch.

---
+++ app/models/order_of_batch.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Float, Integer
+from .base import Base
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+class OrderOfBatch(Base):
+    __tablename__ = "order_of_batch"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    purchase_order_id = Column(UUID(as_uuid=True), ForeignKey = ('purchase_order.id'), unique = False, nullable = False)
+    batch_id = Column(UUID(as_uuid=True), ForeignKey = ('batch.id'), unique = False, nullable = False)
+    price = Column(Float, unique = False, nullable = False)
+    quantity = Column(Integer, unique = False, nullable = False)
+    
+    batch = relationship('Batch')
+    purchase_order = relationship('PurchaseOrder')
