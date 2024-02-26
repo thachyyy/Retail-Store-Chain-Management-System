2694b6a305fc6106d24c5c709985e7380ac83c46
ChauNhan0201 <minhchaunhan01@gmail.com>
2024-02-26 12:27:06 +0700

    [new][26/02/2024] modify model of contract.

--- app/models/contract.py
+++ app/models/contract.py
@@ -1,29 +1,18 @@
-from datetime import datetime
-import enum
-from sqlalchemy import Column, Enum, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Float, Integer, String, DateTime, Boolean
 from sqlalchemy.orm import relationship
-
 from sqlalchemy.dialects.postgresql import UUID
 from .base import Base  # Assuming .base is the correct import path for your Base
-class Status(str,enum.Enum):
-    ACTIVE = "ACTIVE" #Đang kinh doanh
-    INACTIVE = "INACTIVE" #Tạm ngừng kinh doanh
-    PENDING = "PENDING" #Hết hàng
-    EMPTY = "EMPTY" #Trống
-class Promotion(Base):
-    __tablename__ = 'promotion'
+
+class Contract(Base):
+    __tablename__ = 'contract'
 
     id = Column(UUID(as_uuid=True), primary_key=True)
-    promotion_code = Column(String(20), nullable=False, unique=True)
-    promotion_name = Column(String(255), nullable=False)
-    promotion_type = Column(String(255), nullable=False)  
-    promotion_value = Column(Integer, nullable=False)  
-    max_discount_amount = Column(Integer, nullable=False)
-    start_date = Column(DateTime,default=datetime.now(), nullable=False)
-    end_date = Column(DateTime,nullable=False)
-    status = Column(Enum(Status), nullable=False,default=Status.EMPTY)  
-    min_product_value = Column(Integer, nullable=True) 
-    min_product_quantity = Column(Integer, nullable=True)  
-    vendor_id = Column(String(255),ForeignKey=('vendor.id'),nullable=False,unique=False)
+    start_date = Column(DateTime, unique = False, nullable = False)
+    end_date = Column(DateTime, unique = False, nulllable = False)
+    minimum_order_amount = Column(Float, unique = False, nullable = True)
+    minimum_order_quantity = Column(Integer, unique = False, nullable = True)
+    ordering_cycle_amount = Column(Integer, unique = False, nullable = True)
+    ordering_cycle_quantity = Column(Integer, unique = False, nullable = True)
+    belong_to_vendor = Column(UUID(as_uuid=True), ForeignKey = ('vendor.id'), unique = False, nullable = False)
     
     vendor = relationship('Vendor')
