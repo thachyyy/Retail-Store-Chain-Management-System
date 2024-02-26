df3bdfbe13f367f8784e96131921bed24b86e2da
ChauNhan0201 <minhchaunhan01@gmail.com>
2024-02-26 11:48:33 +0700

    [new][26/02/2024] model for order of promotion for order.

---
+++ app/models/promotion_for_order.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Float, Integer
+from .base import Base
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+class PromotionForOrder(Base):
+    __tablename__ = "promotion_for_order"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    promotion_id = Column(UUID(as_uuid=True), ForeignKey = ('promotion.id'), unique = False, nullable = False)
+    purchase_order_id = Column(UUID(as_uuid=True), ForeignKey = ('purchase_order.id'), unique = False, nullable =False)
+    
+    promotion = relationship('Promotion')
+    purchase_order = relationship('PurchaseOrder')
