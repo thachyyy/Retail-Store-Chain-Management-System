6a565548d56267d8e1b1a3c9b7d66c3fd5a28f0e
ChauNhan0201 <minhchaunhan01@gmail.com>
2024-02-26 11:59:32 +0700

    [new][26/02/2024] remove non-use import.

--- app/models/promotion_for_order.py
+++ app/models/promotion_for_order.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Float, Integer
+from sqlalchemy import Column
 from .base import Base
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
