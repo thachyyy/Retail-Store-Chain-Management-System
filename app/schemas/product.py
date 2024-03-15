b1fb4414fef0b2739c3b1fc3ce2cd4a5f835bdd6
thachpham2909 <thachyyy@gmail.com>
2024-03-15 19:40:50 +0700

    update product

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -5,18 +5,17 @@ import enum
 class Status(str,enum.Enum):
     ACTIVE = "ACTIVE" #Đang kinh doanh
     INACTIVE = "INACTIVE" #Tạm ngừng kinh doanh
-    PENDING = "PENDING" #Hết hàng
-    EMPTY = "EMPTY" #Trống
+
 class ProductCreateParams(BaseModel):
     barcode: str = Field(..., max_length=255)
     product_name: str = Field(..., max_length=255)
+    unit: str = Field(..., max_length=255)
+    sale_price: int
+    status: Status = Status.EMPTY
+    last_purchase_price: Optional[int]
     description: Optional[str] = Field(None, max_length=255)
     categories: Optional[str] = Field(None, max_length=255)
     brand: Optional[str] = Field(None, max_length=255)
-    unit: str = Field(..., max_length=255)
-    last_purchase_price: Optional[int]
-    sale_price: int
-    status: Status = Status.EMPTY
     note: Optional[str] = Field(None, max_length=255)
     contract_for_vendor_id: Optional[UUID4] = None
     promotion_id: Optional[UUID4] = None
@@ -26,13 +25,13 @@ class ProductCreateParams(BaseModel):
 class ProductCreate(BaseModel):
     barcode: str 
     product_name: str
-    description: Optional[str]
-    categories: Optional[str] = None 
-    brand: Optional[str] = None 
     unit: str
-    last_purchase_price: Optional[int] = None
     sale_price: int
     status: Status
+    last_purchase_price: Optional[int] = None
+    description: Optional[str] = None
+    categories: Optional[str] = None 
+    brand: Optional[str] = None 
     note: Optional[str] = None 
     contract_for_vendor_id: Optional[UUID4] = None
     promotion_id: Optional[UUID4] = None
@@ -56,7 +55,6 @@ class ProductUpdate(BaseModel):
     has_promotion: Optional[bool]
 
 
-    
 class ProductResponse(ProductCreate):
     class Config:
         orm_mode = True
