6cb54e9d8444d78f9246f9140890e32ad885031f
thachpham2909 <thachyyy@gmail.com>
2024-04-29 22:07:27 +0700

    fix get_sell_throught_rate api

--- app/api/endpoints/dashboard.py
+++ app/api/endpoints/dashboard.py
@@ -248,11 +248,12 @@ async def get_sell_through_rate(
             for order_detail in invoice.order_detail:
                 if order_detail.product_id == id:
                     sold_in_range += order_detail.quantity
-      
-        print("sold_in_range",sold_in_range)
-        print("latest_import",latest_import)
-        sell_rate = (sold_in_range / (latest_import+sold))*100
-        
+        if latest_import > 0:
+            print("sold_in_range",sold_in_range)
+            print("latest_import",latest_import)
+            sell_rate = (sold_in_range / (latest_import+sold))*100
+        else: 
+            sell_rate = 0
             
         new_item = {
             "product_name": product.product_name,
