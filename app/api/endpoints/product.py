1b5d2802912f51a3240057be4afdc7de83db4d49
thachpham2909 <thachyyy@gmail.com>
2024-05-02 11:22:44 +0700

    Add folder dir for specific file

--- app/api/endpoints/product.py
+++ app/api/endpoints/product.py
@@ -19,7 +19,7 @@ from app.schemas.product import ProductCreateParams, ProductUpdate
 from app.services.product import ProductService
 from app.utils.response import make_response_object
 from app.models import Employee
-
+from uuid import uuid4
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
@@ -30,21 +30,28 @@ from app.core.s3 import S3ServiceSingleton  # Adjust the import based on your pr
 
 # Initialize the S3 service singleton
 s3_service = S3ServiceSingleton()
-
+SUPPORTED_FILE_TYPES = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'application/pdf': 'pdf'
+}
 @router.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
     try:
         # Read image file
         file_content = await file.read()
-        # Convert to base64
-        image_base64 = file_content
         
         # Define the file path where the image will be stored on S3
-        file_path = file.filename  # Adjust path as necessary
+        # file_path = file.filename  
 
-        # Upload to S3 and get the public URL
         content_type = file.content_type
-        public_url = s3_service.upload_image_object(image_base64, file_path, content_type)
+        file_extension = SUPPORTED_FILE_TYPES[content_type]
+        if file_extension in ['png', 'jpg']:
+            file_path = f'img/{uuid4()}.{file_extension}'
+        else:
+            file_path = f'pdf/{uuid4()}.{file_extension}'
+        # Upload to S3 and get the public URL
+        public_url = s3_service.upload_image_object(file_content, file_path, content_type)
 
         return JSONResponse(status_code=200, content={"message": "Image uploaded successfully", "url": public_url})
     except Exception as e:
